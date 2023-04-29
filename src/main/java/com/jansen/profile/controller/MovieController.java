8e3705f3e9b256b3c44f1df38392e82bc65a3d19
Andreas Tampubolon <andreasjr61@gmail.com>
2023-04-29 09:38:41 +0700

    add crossOrigin config

--- src/main/java/com/jansen/profile/controller/MovieController.java
+++ src/main/java/com/jansen/profile/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.jansen.profile.controller;
 import com.jansen.profile.common.movie.MovieResponse;
 import com.jansen.profile.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    @CrossOrigin(origins = "https://profile-andreas-be.onrender.com/")
     @GetMapping("/rated")
     public MovieResponse getRatedMovies(){
         return movieService.getRatedMovies();
